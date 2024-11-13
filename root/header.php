eb5665c5bbb61db755152e79ba4c3a9f9e0dcc75
abidomingo <abidomingo25@gmail.com>
2024-11-13 13:24:00 +0800

    Styling modification applied

--- root/header.php
+++ root/header.php
@@ -9,8 +9,7 @@ require_once 'functions.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Subject Management System</title>
-    <!-- Bootstrap CSS -->
+    <title>Student-Subject Management System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
